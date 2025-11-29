c34e621eb9c0b020fac5478f41b644f9a3a5e9e2
nwankwo ifeanyichukwu <nwankwoifeanyi2004@gmail.com>
2025-11-29 12:01:53 +0100

    still changing my codes

--- riceapp/urls.py
+++ riceapp/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('classify/', views.classify_rice_disease, name='classify_rice_disease'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('classify/', views.classify_rice_disease, name='classify_rice_disease'),
     path('history/', views.classification_history, name='classification_history'),
     path('history/<int:result_id>/', views.classification_result_detail, name='classification_result_detail'),
 ]
